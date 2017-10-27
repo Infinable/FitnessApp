a1336a121d8189497e3b621055b754e135a63c76
MooNTiga <pascalpischka@web.de>
2017-10-27 21:59:45 +0200

    Test

--- app/src/main/java/com/android/highlifestudio/fitnessapp/MainActivity.java
+++ app/src/main/java/com/android/highlifestudio/fitnessapp/MainActivity.java
@@ -1,9 +1,22 @@
 package com.android.highlifestudio.fitnessapp;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "XFit";
+    private GoogleApiClient mClient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
 
     void connectGoogleApi()
     {
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        Log.d(TAG,"Conntected to GoogleApi!");
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                            Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                        } else if (i
+                                == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                            Log.i(TAG,
+                                    "Connection lost.  Reason: Service Disconnected");
+                        }
+                    }
+                })
+                .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.d(TAG,"Google Play services connection failed. Cause: " +
+                                connectionResult.toString());
+                        Toast.makeText(MainActivity.this, "Connection to GoogleApi failed!", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .build();
 
     }
 }
