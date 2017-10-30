c8d184a4c8dabd32e7e463dcccc13421761484ab
Angelo <angelo.soltner@stud.uni-due.de>
2017-10-30 02:07:01 +0100

    Miscellaneous

--- app/src/main/java/com/android/highlifestudio/fitnessapp/TrainingActivity.java
+++ app/src/main/java/com/android/highlifestudio/fitnessapp/TrainingActivity.java
@@ -1,13 +1,29 @@
 package com.android.highlifestudio.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrainingActivity extends AppCompatActivity {
 
+    Button startStop;
+    boolean clicked=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+        startStop= (Button) findViewById(R.id.startStopButton);
+        startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked= !clicked;
+                if(clicked)
+                    startStop.setText("Stop");
+                else
+                    startStop.setText("Start");
+            }
+        });
     }
 }
